41e5f0d13b85323adde8982578dd2894bc4fd1bf
izharabbasi <izharabbasi101@gmail.com>
2020-12-28 18:34:57 +0500

    adding array logic

--- Data Structure and algorithm/nemo.js
+++ Data Structure and algorithm/nemo.js
@@ -1,11 +1,16 @@
-const nemo = ['izhar','sheryar','nemo'];
+const {performance} = require('perf_hooks');
+
+const large = new Array(100000).fill('izhar')
 
 function findNemo(arr){
+    let t0 = performance.now();
     for (let i=0; i<arr.length; i++){
-        if(arr[i] === "nemo"){
+        if(arr[i] === "izhar"){
             console.log("Found NEMO");
         }
     }
+    let t1 = performance.now();
+    console.log('the process took ' + (t1 - t0) + ' ms');
 }
 
-findNemo(nemo);
+findNemo(large);
