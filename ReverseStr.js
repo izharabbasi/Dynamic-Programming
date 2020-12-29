d53a7595eba0011c832bc853145ee0666b4cb912
izharabbasi <izharabbasi101@gmail.com>
2020-12-29 19:51:44 +0500

    table pattren

--- ReverseStr.js
+++ ReverseStr.js
@@ -1,21 +1,34 @@
-var str = "hello";
-let reverseStr = str.split('').reverse().join('');
+// var str = "hello";
+// let reverseStr = str.split('').reverse().join('');
 
-console.log(str);
+// console.log(str);
 
-function reverse(str) {
-    if (!str || str.length < 2 || typeof str !== 'string'){
-        return 'thats not good';
+// function reverse(str) {
+//     if (!str || str.length < 2 || typeof str !== 'string'){
+//         return 'thats not good';
+//     }
+
+//     const backward = [];
+//     let totalItem = str.length-1;
+
+//     for (let i = totalItem; i >= 0; i--){
+//         backward.push(str[i])
+//     }
+
+//     return backward.join('');
+// }
+
+// console.log(reverse('JavaScipt progamming'));
+
+function multiTable(number) {
+    // good luck
+        const table = []
+        for (i=0; i <= 10; i++){
+            table.push(`${number} * ${i} = ${number*i}`)
+        }
+        return table.join('\n')
     }
+    
 
-    const backward = [];
-    let totalItem = str.length-1;
+console.log(multiTable(100));
 
-    for (let i = totalItem; i >= 0; i--){
-        backward.push(str[i])
-    }
-
-    return backward.join('');
-}
-
-console.log(reverse('JavaScipt progamming'));
