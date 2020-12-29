e0fa316f944cc33c643779ea723d3f60c16ee5e4
izharabbasi <izharabbasi101@gmail.com>
2020-12-29 23:59:14 +0500

    todays last logic

--- Data Structure and algorithm/practice.js
+++ Data Structure and algorithm/practice.js
@@ -1,15 +1,32 @@
-function getAverage(marks){
-    //TODO : calculate the downwar rounded average of the marks array
-    let total = 0;
-    for (let i=0; i < marks.length; i ++){
-      total += marks[i];
+// function getAverage(marks){
+//     //TODO : calculate the downwar rounded average of the marks array
+//     let total = 0;
+//     for (let i=0; i < marks.length; i ++){
+//       total += marks[i];
+//     }
+//     console.log(marks.length);
+//     let avg = total / marks.length;
+    
+//       return avg
+    
+//   }
+
+
+// console.log(getAverage([1,2,3,4,5,]));
+
+
+function divisibleByLast(n) {
+    // TODO
+    const newArray = [];
+    let values = 1;
+    for (i=values ; i <= n.length; i++){
+      if(values[i] / n[i]){
+          newArray.push(true)
+      } else {
+        newArray.push(true)
+      }
     }
-    console.log(marks.length);
-    let avg = total / marks.length;
-    
-      return avg
-    
+    return newArray;
   }
 
-
-console.log(getAverage([1,2,3,4,5,]));
+console.log(divisibleByLast(1337));
