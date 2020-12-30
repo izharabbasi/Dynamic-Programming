653a70e3bd83a632084c4d83d5e4c7a639f976f6
izharabbasi <izharabbasi101@gmail.com>
2020-12-30 20:29:35 +0500

    addedd 2nd exercide

--- Javascript Exercises/exercise1.js
+++ Javascript Exercises/exercise1.js
@@ -1,7 +1,16 @@
 //Excercise No 1
 
-const getFileExtention = (str) => {
-    return str.slice(str.lastIndexOf('.'));
+// const getFileExtention = (str) => {
+//     return str.slice(str.lastIndexOf('.'));
+// }
+
+// console.log(getFileExtention("hello.html"));
+
+//Excercise No 2
+
+function newString (str) {
+    return str.indexOf('New!') === 0 ? str : `New! ${str}`
+
 }
 
-console.log(getFileExtention("hello.html"));
+console.log(newString('New! Izhar'));
