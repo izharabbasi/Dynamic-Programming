19e1d6cb5fd36481de4c44079657b8a70d7be936
izharabbasi <izharabbasi101@gmail.com>
2021-01-01 20:44:32 +0500

    complete hashTable logic

--- Data Structure and algorithm/hashTable.js
+++ Data Structure and algorithm/hashTable.js
@@ -11,11 +11,43 @@ class HashTable {
         return hash;
     }
 
-    
+    set(key, value) {
+        let address = this._hash(key);
+        if(!this.data[address]){
+            this.data[address] = []
+        }
+
+        this.data[address].push([key,value])
+        return this.data
+    }
+
+    get(key){
+        let address = this._hash(key);
+        let currentBucket = this.data[address];
+        if(currentBucket){
+            for (let i = 0; i < currentBucket.length; i++){
+                if (currentBucket[i][0] === key){
+                    return currentBucket[i][1]
+                }
+            }
+        }
+        return undefined;
+    }
+    keys(){
+        const keyArray = [];
+        for (let i = 0; i < this.data.length; i++) {
+            if(this.data[i]){
+                keyArray.push(this.data[i][0][0])
+            }
+        }
+        return keyArray;
+    }
 }
-
-
 
 const myHashTable = new HashTable(50);
 myHashTable.set('grapes' , 10000);
-myHashTable.get('grapes');
+myHashTable.set('apples' , 10000);
+myHashTable.set('banana' , 10000);
+myHashTable.set('apples' , 10000);
+console.log(myHashTable.keys());
+
