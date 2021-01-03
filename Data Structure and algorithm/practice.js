d3d8d99437eff39af62f8f02f1fef5941779c37d
izharabbasi <izharabbasi101@gmail.com>
2021-01-03 23:50:18 +0500

    uncomment

--- Data Structure and algorithm/practice.js
+++ Data Structure and algorithm/practice.js
@@ -15,21 +15,21 @@
 // console.log(getAverage([1,2,3,4,5,]));
 
 
-// function divisibleByLast(n) {
-//     // TODO
-//     const newArray = [];
-//     let values = 1;
-//     for (i=values ; i <= n.length; i++){
-//       if(values[i] / n[i]){
-//           newArray.push(true)
-//       } else {
-//         newArray.push(true)
-//       }
-//     }
-//     return newArray;
-//   }
+function divisibleByLast(n) {
+    // TODO
+    const newArray = [];
+    let values = 1;
+    for (i=values ; i <= n.length; i++){
+      if(values[i] / n[i]){
+          newArray.push(true)
+      } else {
+        newArray.push(true)
+      }
+    }
+    return newArray;
+  }
 
-// console.log(divisibleByLast(1337));
+console.log(divisibleByLast(1337));
 
 
 function sortedArray(arr1, arr2){
