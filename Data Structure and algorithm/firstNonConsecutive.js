2a102ec27bd43cb00fe3e4a1b534fa34dcf31d5b
izharabbasi <izharabbasi101@gmail.com>
2021-01-03 23:51:20 +0500

    uncomment

--- Data Structure and algorithm/firstNonConsecutive.js
+++ Data Structure and algorithm/firstNonConsecutive.js
@@ -1,23 +1,14 @@
-// function firstNonConsecutive (arr) {
-//     for (var i = 1; i < arr.length; i++) {
-//       var cur = arr[i];
-//       var prev = arr[i-1];
+function firstNonConsecutive (arr) {
+    for (var i = 1; i < arr.length; i++) {
+      var cur = arr[i];
+      var prev = arr[i-1];
       
-//       if (cur - prev > 1) {
-//         return cur;
-//       }
-//     }
-//     return null;
-//   }
+      if (cur - prev > 1) {
+        return cur;
+      }
+    }
+    return null;
+  }
 
 
-// console.log(firstNonConsecutive([1,2,3,4,5,7,8]));
-
-function buildRowText(index, character) {
-    // Do your magic :
-
-    
-
-}
-
-console.log(buildRowText(2, 'A'));
+console.log(firstNonConsecutive([1,2,3,4,5,7,8]));
