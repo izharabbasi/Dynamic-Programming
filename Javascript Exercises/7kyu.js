ede4c58e3af52eaa09af9d2c3eddc4f0ffade7bb
izharabbasi <izharabbasi101@gmail.com>
2021-01-05 17:56:50 +0500

    filter_list logic

--- Javascript Exercises/7kyu.js
+++ Javascript Exercises/7kyu.js
@@ -1,11 +1,11 @@
-function highAndLow(numbers) {
-  // ...
-    numbers = numbers.split(' ');
-    return Math.max.apply(null, numbers) + " " +  Math.min.apply(null, numbers)
-}
+// function highAndLow(numbers) {
+//   // ...
+//     numbers = numbers.split(' ');
+//     return Math.max.apply(null, numbers) + " " +  Math.min.apply(null, numbers)
+// }
 
 
-console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
+// console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
 
 // function descendingOrder(n){
 //     let numbers = n.toString().split('');
@@ -15,3 +15,17 @@ console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
 // }
 
 // console.log(descendingOrder(123456789));
+
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    const newArray =[];
+    for(let i =0; i < l.length; i++){
+        if(typeof(l[i]) === 'number'){
+            newArray.push(l[i])
+        }
+    }
+    return newArray;
+}
+
+console.log(filter_list([1,2,'a','b']));
+console.log(filter_list([1,2,'aasf','1','123',123]));
