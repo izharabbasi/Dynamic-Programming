a13b9240c9ddb87c1d4fc2b2777e93242a11ad72
izharabbasi <izharabbasi101@gmail.com>
2021-01-05 17:01:01 +0500

    descendingOrder logic

---
+++ Javascript Exercises/7kyu.js
@@ -0,0 +1,17 @@
+// function highAndLow(numbers) {
+//   // ...
+//     numbers = numbers.split(' ');
+//     return Math.max.apply(null, numbers) + " " +  Math.min.apply(null, numbers)
+// }
+
+
+// console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
+
+function descendingOrder(n){
+    let numbers = n.toString().split('');
+    return [...numbers].sort((a,b) => b-a).join('');
+
+    
+}
+
+console.log(descendingOrder(123456789));
