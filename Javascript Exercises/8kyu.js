4154a7ac9098b4e09d21dda527ae3904d3aa3eac
izharabbasi <izharabbasi101@gmail.com>
2021-01-06 16:48:51 +0500

    getPlanetName logic

--- Javascript Exercises/8kyu.js
+++ Javascript Exercises/8kyu.js
@@ -35,14 +35,41 @@
 
 // console.log(countPositivesSumNegatives(testData));
 
-function areYouPlayingBanjo(name) {
-    // Implement me
-    if(name.charAt(0) === "R"){
-        return `${name} plays banjo`;
+// function areYouPlayingBanjo(name) {
+//     // Implement me
+//     if(name.charAt(0) === "R"){
+//         return `${name} plays banjo`;
+//     }
+
+//     return `${name} does not play banjo`;
+// }
+
+// console.log(areYouPlayingBanjo("Martin"));
+// console.log(areYouPlayingBanjo("Rikke"));
+
+function getPlanetName(id){
+    var name;
+    switch(id){
+      case 1:
+        return name = 'Mercury'
+      case 2:
+        return name = 'Venus'
+      case 3:
+        return name = 'Earth'
+      case 4:
+        return name = 'Mars'
+      case 5:
+        return name = 'Jupiter'
+      case 6:
+        name = 'Saturn'
+      case 7:
+        return name = 'Uranus'
+      case 8:
+        return name = 'Neptune'
     }
+    
+  }
 
-    return `${name} does not play banjo`;
-}
-
-console.log(areYouPlayingBanjo("Martin"));
-console.log(areYouPlayingBanjo("Rikke"));
+console.log(getPlanetName(2));
+console.log(getPlanetName(5));
+console.log(getPlanetName(3));
