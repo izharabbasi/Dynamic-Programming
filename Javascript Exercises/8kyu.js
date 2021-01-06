ec4cc698a33456a6b2739e297356b9aced333ee9
izharabbasi <izharabbasi101@gmail.com>
2021-01-06 17:03:21 +0500

    sumArray logic

--- Javascript Exercises/8kyu.js
+++ Javascript Exercises/8kyu.js
@@ -47,29 +47,46 @@
 // console.log(areYouPlayingBanjo("Martin"));
 // console.log(areYouPlayingBanjo("Rikke"));
 
-function getPlanetName(id){
-    var name;
-    switch(id){
-      case 1:
-        return name = 'Mercury'
-      case 2:
-        return name = 'Venus'
-      case 3:
-        return name = 'Earth'
-      case 4:
-        return name = 'Mars'
-      case 5:
-        return name = 'Jupiter'
-      case 6:
-        name = 'Saturn'
-      case 7:
-        return name = 'Uranus'
-      case 8:
-        return name = 'Neptune'
-    }
+// function getPlanetName(id){
+//     var name;
+//     switch(id){
+//       case 1:
+//         return name = 'Mercury'
+//       case 2:
+//         return name = 'Venus'
+//       case 3:
+//         return name = 'Earth'
+//       case 4:
+//         return name = 'Mars'
+//       case 5:
+//         return name = 'Jupiter'
+//       case 6:
+//         name = 'Saturn'
+//       case 7:
+//         return name = 'Uranus'
+//       case 8:
+//         return name = 'Neptune'
+//     }
     
-  }
+//   }
 
-console.log(getPlanetName(2));
-console.log(getPlanetName(5));
-console.log(getPlanetName(3));
+// console.log(getPlanetName(2));
+// console.log(getPlanetName(5));
+// console.log(getPlanetName(3));
+
+
+function sumArray(array) {
+    const newArray = [];
+    if(array === null || array.length <= 1){
+        return array
+    }
+    let max = Math.max(...array);
+    let min = Math.min(...array);
+    let sum = 0;
+    for(let i=0; i < array.length; i++){
+        sum += array[i]
+    }
+    return sum -max - min;
+}
+
+console.log(sumArray([ 6, 2, 1, 8, 10 ]));
