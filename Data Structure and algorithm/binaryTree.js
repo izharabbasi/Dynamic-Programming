2d035d6c75f7b89c674f07761419e20a275b9dfc
izharabbasi <izharabbasi101@gmail.com>
2021-01-09 22:12:19 +0500

    BinarySearchTree logic

--- Data Structure and algorithm/binaryTree.js
+++ Data Structure and algorithm/binaryTree.js
@@ -1,23 +1,23 @@
-// class Node {
-//     constructor(value){
-//         this.left = null;
-//         this.right = null;
-//         this. value = value
-//     }
-// }
+class Node {
+    constructor(value){
+        this.left = null;
+        this.right = null;
+        this. value = value
+    }
+}
 
 
 
-// class BinarySearchTree {
-//     constructor(){
-//         this.root = null;
-//     }
-//     insert(value){
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
 
-//     }
-//     lookup(value){
+    }
+    lookup(value){
 
-//     }
-// }
+    }
+}
 
-// const tree = new BinarySearchTree();
+const tree = new BinarySearchTree();
