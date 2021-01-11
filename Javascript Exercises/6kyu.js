b2daf69664948f696cb452fbd512e27eac82e30e
izharabbasi <izharabbasi101@gmail.com>
2021-01-11 20:18:44 +0500

    orderBreaker improve logic

--- Javascript Exercises/6kyu.js
+++ Javascript Exercises/6kyu.js
@@ -1,21 +1,5 @@
 function orderBreaker(input) {
-    //your code
-    var sortedArray = [...input].sort((a, b)=>a-b)
     
-    for(i=0; i<input.length; i++){
-
-        if(sortedArray[i] !== input[i]){
-        
-            if(input.indexOf(sortedArray[0]) > 1){
-                return sortedArray[0]
-            }
-        
-            else if(sortedArray[i] < input[i]){
-                return input[i];
-            }  
-        
-        }
-    }
 }
 
 console.log(orderBreaker([1,2,3,4,17,5,6,7,8]));
