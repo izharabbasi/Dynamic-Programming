d51dd213b0ecc29310fdf83acec8c2a18dd476d3
izharabbasi <izharabbasi101@gmail.com>
2021-01-11 20:26:05 +0500

    orderBreaker logic

--- Javascript Exercises/6kyu.js
+++ Javascript Exercises/6kyu.js
@@ -1,5 +1,18 @@
 function orderBreaker(input) {
-    
+    const sortedArray = [...input].sort((a ,b) => a - b);
+    console.log(sortedArray);
+
+    for(let i =0; i < input.length; i++){
+        
+        if(sortedArray[i] !== input[i]){
+            if(input.indexOf(sortedArray[0]) > 1){
+                return sortedArray[0]
+            }
+            else if (sortedArray[i] < input[i]){
+                return input[i]
+            }
+        }
+    }
 }
 
 console.log(orderBreaker([1,2,3,4,17,5,6,7,8]));
