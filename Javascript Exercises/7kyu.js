130b68eb06ed9a9e1ec10a51e7e6a6418841f109
izharabbasi <izharabbasi101@gmail.com>
2021-01-15 20:25:44 +0500

    improve logic

--- Javascript Exercises/7kyu.js
+++ Javascript Exercises/7kyu.js
@@ -715,14 +715,8 @@
 // console.log(solve([3,4,4,3,6,3]));
 
 function flattenAndSort(array) {
-    // Good luck, brave code warrior!
-    const newArray = []
-    for(let i =0 ; i< array.length; i++ ){
-        newArray.push(array[i].sort())
-    }
-    let sort = newArray.sort();
-
-    return [].concat(...sort).sort((a,b) => a - b)
+    // Good luck, brave code warrior
+    return [].concat(...array).sort((a,b) => a - b)
 }
 
 console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]));
